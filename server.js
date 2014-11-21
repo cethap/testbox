a936631978365357f37b228a60af7cbf6a52c4c0
cethapgames@gmail.com <cethapgames@gmail.com>
2014-11-21 00:04:36 -0500

    Traducción

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 var http = require('http');
 http.createServer(function (req, res) {
   res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.end('Hello World\n');
+  res.end('Hola Mundo\n');
 }).listen(1337, '127.0.0.1');
-console.log('Server running at http://127.0.0.1:1337/');
+console.log('Servdor correindo en http://127.0.0.1:1337/');
